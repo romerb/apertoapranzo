29cc6914ccfb46cbd480ba7fbdbb12d13b649c12
Giuseppe Modarelli <giuseppe.modarelli@gmail.com>
2015-04-27 20:46:24 +0200

    Add back the foursquare gem, fix the secrets.yml.sample

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,11 +3,15 @@ class PagesController < ApplicationController
   end
 
   def search
-    uri = URI("https://api.foursquare.com/v2/venues/search?client_id=#{Rails.application.secrets.foursq_client_id}&client_secret=#{Rails.application.secrets.foursq_client_secret}&ll=#{params[:lat]},#{params[:lon]}&v=20120609")
+    render json: client.search_venues(ll: "#{params[:lat]},#{params[:lon]}")
+  end
 
-    @response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
-      http.request Net::HTTP::Get.new uri
-    end
-    render json: @response.body
+  private
+  def client
+    Foursquare2::Client.new(
+      client_id: Rails.application.secrets.foursq_client_id,
+      client_secret: Rails.application.secrets.foursq_client_secret,
+      api_version: '20150427'
+    )
   end
 end
