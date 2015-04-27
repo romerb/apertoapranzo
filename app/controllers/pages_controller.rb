fb8ae2334c0e4ef172be57eb2c89aacf00e425db
Giuseppe Modarelli <giuseppe.modarelli@gmail.com>
2015-04-27 20:39:45 +0200

    Call the Foursquare api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,13 @@
 class PagesController < ApplicationController
   def home
-    
   end
 
   def search
-    puts Rails.application.secrets
-    client = Foursquare2::Client.new(client_id: Rails.application.secrets.foursq_client_id, client_secret: Rails.application.secrets.foursq_client_id, :api_version => '20120609')
-    client.search_venues(ll: "#{params[:lat]},#{params[:lon]}")
-  end 
+    uri = URI("https://api.foursquare.com/v2/venues/search?client_id=#{Rails.application.secrets.foursq_client_id}&client_secret=#{Rails.application.secrets.foursq_client_secret}&ll=#{params[:lat]},#{params[:lon]}&v=20120609")
 
+    @response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+      http.request Net::HTTP::Get.new uri
+    end
+    render json: @response.body
+  end
 end
